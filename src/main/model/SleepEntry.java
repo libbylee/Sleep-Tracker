f141188b04c7694e443760e07fbf0574ea5ed116
llee30 <llee30@student.ubc.ca>
2024-10-06 18:04:26 -0700

    Added specification for SleepEntry class

---
+++ src/main/model/SleepEntry.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.time.LocalDate;
+
+//Represents a single sleep journal entry having a date, the hours slept, quality, and notes about the sleep 
+public class SleepEntry {
+    private LocalDate date; // the date of the sleep entry
+    private double hoursSlept; //the number of hours slept
+    private int sleepRating; //the quality of sleep rated on a scale from 1-10
+    private String sleepNote; // Notes about the sleep
+
+    //REQUIRES: sleepRating >=0, sleepNote has a non=zero length
+    //MODIFIES: this 
+    //EFFECTS: sleepentry is given a date, numbers of hours slept, a rating, and a note
+
+    public SleepEntry(double hoursSlept, int sleepRating, String sleepNote) {
+        this.date = LocalDate.now();
+        this.hoursSlept = hoursSlept; 
+        this.sleepRating = sleepRating; 
+        this.sleepNote = sleepNote;
+    }
+}
