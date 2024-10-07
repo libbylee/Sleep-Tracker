60f8dc8515cc181406dd77b169c0f0880da35234
llee30 <llee30@student.ubc.ca>
2024-10-06 20:53:09 -0700

    Added test code coverage for SleepEntry class

--- src/test/model/SleepEntryTest.java
+++ src/test/model/SleepEntryTest.java
@@ -3,6 +3,7 @@ package model;
 // import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,17 +11,38 @@ import org.junit.jupiter.api.Test;
 public class SleepEntryTest {
 
     private SleepEntry sleepEntryTest; 
+    private LocalDate date; 
+    private double hoursSlept; 
+    private int sleepRating; 
+    private String sleepNote;
     
     @BeforeEach
     void runBefore() {
-    sleepEntryTest = new SleepEntry(1.0, 5,"test");
+        sleepEntryTest = new SleepEntry(LocalDate.of(2024,1,1), 0.0, 0, "test"); 
     }
 
     @Test
     void testConstructor() {
-        assertEquals(1.0, sleepEntryTest.getHoursSlept());
-        assertEquals(5, sleepEntryTest.getSleepRating());
+        assertEquals((LocalDate.of(2024, 1, 1)), sleepEntryTest.getDate());
+        assertEquals(0.0, sleepEntryTest.getHoursSlept());
+        assertEquals(0, sleepEntryTest.getSleepRating());
         assertEquals("test", sleepEntryTest.getSleepNote());
+    }
+
+    @Test
+    void testSetSleepNote() {
+        sleepEntryTest.setSleepNote("This is a test.");
+        assertEquals("This is a test.", sleepEntryTest.getSleepNote());
+        sleepEntryTest.setSleepNote("This is a second test.");
+        assertEquals("This is a second test.", sleepEntryTest.getSleepNote());
+    }
+
+    @Test
+    void testSetSleepRating() {
+        sleepEntryTest.setSleepRating(3);
+        assertEquals(3, sleepEntryTest.getSleepRating());
+        sleepEntryTest.setSleepRating(9);
+        assertEquals(9, sleepEntryTest.getSleepRating());
     }
 
 }
