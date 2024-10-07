7e7a15690e830f7b4e811d22ea2c65488e6e1ca3
llee30 <llee30@student.ubc.ca>
2024-10-06 19:55:37 -0700

    Added size and getEntry method for SleepEntry

--- src/main/model/SleepJournal.java
+++ src/main/model/SleepJournal.java
@@ -4,24 +4,40 @@ import java.util.ArrayList;
 
 //shows a list of all sleep entries
 public class SleepJournal {
-    private ArrayList<SleepEntry> sleepEntries; 
+    private ArrayList<SleepEntry> sleepJournal; 
 
     //EFFECTS: makes an empty list, initializes the list 
     public SleepJournal(){
-        sleepEntries = new ArrayList<>(); 
+        sleepJournal = new ArrayList<>(); 
     }
 
     //REQUIRES: SleepEntry != null
     //MODIFIES: this
     //EFFECTS: adds a new sleep entry to the journal
     public void addSleepEntryToSleepJournal(SleepEntry entry){
-        sleepEntries.add(entry);
+        sleepJournal.add(entry);
     }
+
     //REQUIRES: index number is a valid index in the SleepEntry list
     //MODIFIES: this
     //EFFECTS: removes a sleep entry in the journal by index
     public void removeSleepEntryByIndex(int index){
-        index = 0;
+        if (index >= 0 && index <= sleepJournal.size())
+        sleepJournal.remove(index);
+        else{
+            System.out.println("Invalid");
+        }
+    }
+
+    //REQUIRES: index number is a valid index in the SleepEntry list
+    //EFFECTS: returns the sleep entry at the specified index
+    public SleepEntry getEntry(int index) {
+        return sleepJournal.get(index);
+    }
+    
+    //EFFECTS: returns the number of sleep entries
+    public int size() {
+        return sleepJournal.size();
     }
 }
 
