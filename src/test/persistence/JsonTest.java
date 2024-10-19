101420a67a2ed35e03043761b7673b4ff8c7b0b3
llee30 <llee30@student.ubc.ca>
2024-10-18 22:25:35 -0700

    Added tests for JsonWriter

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 import model.SleepEntry;
 
 public class JsonTest {
-    protected void checkSleepEntry(LocalDate date, double hoursSlept, int sleepRating, String sleepNote, SleepEntry entry) {
-        assertEquals(date, entry.getDate());
+    protected void checkSleepEntry(String date, double hoursSlept, int sleepRating, String sleepNote, SleepEntry entry) {
+        assertEquals(LocalDate.parse(date), entry.getDate());
         assertEquals(hoursSlept, entry.getHoursSlept());
         assertEquals(sleepRating, entry.getSleepRating());
         assertEquals(sleepNote, entry.getSleepNote());
---
+++ src/test/persistence/JsonWriterTest.java
@@ -0,0 +1,80 @@
+package persistence;
+
+import org.junit.jupiter.api.Test;
+
+import model.SleepJournal;
+import model.SleepEntry;
+
+import java.time.LocalDate;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonWriterTest extends JsonTest {
+    // the strategy in designing tests for the JsonWriter is to
+    // write data to a file and then use the reader to read it back in and check
+    // that we
+    // read in a copy of what was written out.
+    private SleepEntry entry1;
+    private SleepEntry entry2;
+    private SleepEntry entry3;
+
+    @Test
+    void testWriterInvalidFile() {
+
+        try {
+            JsonWriter writer = new JsonWriter(".data/noSuchFile.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testWriterEmptySleepJournal() {
+        try {
+            SleepJournal sj = new SleepJournal();
+            JsonWriter writer = new JsonWriter(".data/testWriterEmptySleepJournal.json");
+            writer.open();
+            writer.write(sj);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptySleepJournal.json");
+            sj = reader.read();
+            assertEquals(0, sj.size());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterGeneralSleepJournal() {
+        try {
+            SleepJournal sj = new SleepJournal();
+            SleepEntry entry1 = new SleepEntry(LocalDate.parse("2024-10-09"), 2, 5, "This was a terrible sleep");
+            SleepEntry entry2 = new SleepEntry(LocalDate.parse("2024-10-10"), 10, 10, "10/10 sleep for a 10/10 day");
+            SleepEntry entry3 = new SleepEntry(LocalDate.parse("2024-10-11"), 7.5, 6, "Not bad");
+            sj.addSleepEntryToSleepJournal(entry1);
+            sj.addSleepEntryToSleepJournal(entry2);
+            sj.addSleepEntryToSleepJournal(entry3);
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralSleepJouranl.json");
+            writer.open();
+            writer.write(sj);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralSleepJournal.json");
+            sj = reader.read();
+            List<SleepEntry> entries = sj.getAllEntries();
+            assertEquals(3, entries.size());
+            checkSleepEntry("2024-10-09", 2, 5, "This was a terrible sleep", entries.get(0));
+            checkSleepEntry("2024-10-10", 10, 10, "10/10 sleep for a 10/10 day", entries.get(1));
+            checkSleepEntry("2024-10-11", 7.5, 6, "Not bad", entries.get(2));
+
+        } catch (Exception e) {
+            fail("Exception should not have been thrown");
+        }
+
+    }
+}
